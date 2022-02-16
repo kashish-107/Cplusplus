b753462f41dd903691bb8c0e2369ab78944cffc3
kashish-107 <kashish.mittal107@gmail.com>
2022-02-16 19:31:39 +0530

    Copy ctor update

--- BasicWorld/copyCtor_Vs_assignmentOp.cpp
+++ BasicWorld/copyCtor_Vs_assignmentOp.cpp
@@ -1,57 +1,59 @@
 #include<iostream>
 #include<memory>
+#include<string.h>
 using namespace std;
 
 class test
 {
 public:
-	test() {
-		ptr = nullptr;	
-	}
-	test(int i) {
-		ptr= new int(i);
+	test():_ip{nullptr}, _cp{nullptr} { }
+
+	test(int i, const char *str = nullptr) {
+	        _ip = new int(i);	
+		_cp = new char[strlen(str)+1];
+		strcpy(_cp, str);
 	}
 
 	test(const test& obj) {
 		cout<<"Copy ctor"<<endl;
-		ptr = new int(0);
-		*ptr=*(obj.ptr);
+		_ip = new int(*obj._ip);
+		_cp = new char[strlen(obj._cp)+1];
+		strcpy(_cp, obj._cp);
+	}
+
+	test& operator= (const test& obj) {
+		cout<<"Assignment operator"<<endl;
+		if(!_ip) 
+			_ip = new int(0);
+		*_ip = *(obj._ip);
+		return *this;
 	}
 
 	void show() {
-		cout<<"Value="<<*ptr<<endl;
-	}
-
-	void updateVal() {
-		*ptr=20;
-	}
-	
-	test& operator= (const test& obj) {
-		cout<<"Assignment operator"<<endl;
-		if(!ptr) 
-			ptr = new int(0);
-		*ptr = *(obj.ptr);
-		return *this;
+		cout<<"Value="<<*_ip<<endl;
+		cout<<"String="<<_cp<<endl;
 	}
 
 	~test() {
 		cout<<"Dtor"<<endl;
-		delete ptr;
+		delete _ip;
+		delete [] _cp;
 	}
 private:
-	int* ptr;
+	int* _ip;
+	char* _cp;
+	int size{0};
 };
 
 int main()
 {
-	test t1(10);
+	test t1(10, "abc");
 	test t2 = t1;
 	t1.show();
 	t2.show();
 
 	test t3;
 	t3 = t2;
-	t3.updateVal();
 	t2.show();	
 	return 0;
 }
